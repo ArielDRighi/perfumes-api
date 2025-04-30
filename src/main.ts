078efe1a47d07a7e4d0945ac1d13c0eb4cde33a5
Ariel Righi <arieldavidrighi@gmail.com>
2025-04-30 16:02:15 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configurar CORS para el frontend en el puerto 3001
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Perfumes API')
     .setDescription('API documentation for Perfumes application')
